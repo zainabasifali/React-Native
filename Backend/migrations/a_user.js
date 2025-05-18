1994ef217e6e367fec856af6f10bc3cb5db7e32b
zainabasifali <zainabasif5312@gmail.com>
2025-05-18 16:41:11 +0500

    updated migration of user

--- Backend/migrations/a_user.js
+++ Backend/migrations/a_user.js
@@ -10,6 +10,7 @@ exports.up = function(knex) {
         table.string('email').notNullable().unique();
         table.string('password').notNullable();
         table.string('profilePicture').defaultTo(null);
+        table.string('role').defaultTo("user");
         table.string('profession').defaultTo(null);
         table.integer('follower_count').defaultTo(0);
         table.integer('following_count').defaultTo(0);
